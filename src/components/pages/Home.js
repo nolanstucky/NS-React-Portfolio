9d20311f963901ba142de3240f8507ff738ac81e
Nolan Stucky <nolan@stucky.net>
2021-02-22 22:22:30 -0800

    added particles to website

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Main, Grid, Box } from 'grommet';
 import Profile from '../profile/index'
-
+import Particles from "react-tsparticles";
+import particlesOptions from "../particles/particles.json"
 
 function Home() {
     return (
         <Main>
+            <Particles options={particlesOptions}/>
             <Box align="center">
                 <Profile />
             </Box>
