917e620b1d64e8dbad2cc5e392dafc8473905f8d
Nolan Stucky <nolan@stucky.net>
2021-03-05 00:23:42 -0800

    fixed readme

--- README.md
+++ README.md
@@ -1,70 +1,14 @@
-# Getting Started with Create React App
+# My Portfolio
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+## Description:
+This is my portfolio created with React using Grommet React Framework. It has my information on me as a web developer and also a list of my projects with links to all of their repos and deployed sites with a description as well. I hope you enjoy the serene setting of the mountains!
 
-## Available Scripts
+## Heroku Link:
+https://nsbest-react-portfolio.herokuapp.com/
 
-In the project directory, you can run:
+## Contributing:
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](https://www.contributor-covenant.org/version/2/0/code_of_conduct/)
 
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+## Questions:
+* Email: <nolan@stucky.net>
+* GitHub Profile: [nolanstucky](https://github.com/nolanstucky)
--- src/data.js
+++ src/data.js
@@ -1,43 +1,43 @@
 const projectData = [
     {
         projectTitle: "Check Your Fridge",
-        image: "/assets/check-your-fridge.png",
+        image: "https://github.com/nolanstucky/NS-React-Portfolio/blob/main/public/assets/check-your-fridge.png?raw=true",
         description: "Check Your Fridge creates a database of food that you have in your fridge. Using Spoonacular API, you can find recipes that use specific ingredients to help you use your food before it expires",
         deployedLink: "https://cryptic-shore-39350.herokuapp.com/",
         githubLink: "https://github.com/lbalbrecht/check-your-fridge"
     },
     {
         projectTitle: "PokeBatEx",
-        image: "/assets/pokebatex.png",
+        image: "https://github.com/nolanstucky/NS-React-Portfolio/blob/main/public/assets/pokebatex.png?raw=true",
         description: "Have you ever felt like you suck at pokemon battles? Then you should use PokeBatEx, a pokemon type trainer to learn the type advantages of pokemon while having fun!",
         deployedLink: "https://tyrannofloresrex.github.io/PokeBatEx/",
         githubLink: "https://github.com/nolanstucky/PokeBatEx"
     },
     {
         projectTitle: "Note Taker",
-        image: "/assets/note-taker2.png",
-        description: "Check Your Fridge creates a database of food that you have in your fridge. Using Spoonacular API, you can find recipes that use specific ingredients to help you use your food before it expires",
+        image: "https://github.com/nolanstucky/NS-React-Portfolio/blob/main/public/assets/note-taker2.png?raw=true",
+        description: "An application hosted with heroku that allows the user to start taking notes by clicking on a get started button. The user is transfered to the note page which they are able to write notes with a title and text for a single note. They are also able to save the note to the server. Additionally they can get rid of any note by clicking on a red trash can deleting the note permently.",
         deployedLink: "https://ns-note-taker.herokuapp.com",
         githubLink: "https://github.com/nolanstucky/Note-Taker"
     },
     {
         projectTitle: "Knowledge4Hire",
-        image: "/assets/knowledge4higher.png",
-        description: "Knowledge4Hire is a website that allowing users to ",
+        image: "https://github.com/nolanstucky/NS-React-Portfolio/blob/main/public/assets/knowledge4higher.png?raw=true",
+        description: "Knowledge4Hire is a website that allowing users to come together to share knowledge and information. Or if someone needs more than just there questions answered they can implore sertified exports on there fields and buy services from them! ",
         deployedLink: "https://knowledge4hire.herokuapp.com/splash",
         githubLink: "https://github.com/Drantho/KnowledgeForHigher"
     },
     {
         projectTitle: "Employee Directory",
-        image: "/assets/demonstrationemployeedirectory.png",
+        image: "https://github.com/nolanstucky/NS-React-Portfolio/blob/main/public/assets/demonstrationemployeedirectory.png?raw=true",
         description: "This is a web aplication powered by react.js that can be used to show employee or user data that is easily sorted by first name and sorted by date of birth. Additionally the use of searching employee data by first name is also implemented.",
         deployedLink: "https://cryptic-shore-39350.herokuapp.com/",
         githubLink: "https://github.com/nolanstucky/Employee-Directory-"
     },
     {
         projectTitle: "README Generator",
-        image: "/assets/README-Demonstration.gif",
-        description: "Check Your Fridge creates a database of food that you have in your fridge. Using Spoonacular API, you can find recipes that use specific ingredients to help you use your food before it expires",
+        image: "https://github.com/nolanstucky/NS-React-Portfolio/blob/main/public/assets/README-Demonstration.gif?raw=true",
+        description: "A command-line application using node.js to create a README.md file based off of the user's input. The file also includes github username, email, licensing badge and an optional Contributor Covenant Badge.",
         deployedLink: "",
         githubLink: "https://github.com/nolanstucky/README-Generator"
     },
