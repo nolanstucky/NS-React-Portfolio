49ac36dde5dcb1f093f52b64f998a9fbe958bbc3
Nolan Stucky <nolan@stucky.net>
2021-03-04 20:04:21 -0800

    finished main body and updated links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-import { Footer, Text} from 'grommet';
+import { Footer, Text, Anchor} from 'grommet';
 import './style.css'
+import { Github, Linkedin} from 'grommet-icons';
 class Footbar extends Component {
     render() {
         return (
             <Footer background="light-4" justify="center" pad="small" className="footer">
                 <Text textAlign="center" size="small">
-                    © 2019 Copyright Grommet
+                <Anchor href="https://github.com/nolanstucky?tab=repositories"  color="white"> <Github/></Anchor>
+                <Anchor href="https://www.linkedin.com/in/nolan-stucky/" color="white"><Linkedin/></Anchor>
             </Text>
             </Footer>
 
--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
-import { Box, Avatar, Anchor, Grid, Text } from 'grommet';
+import { Box, Avatar, Anchor, Grid, Text, Heading} from 'grommet';
 import './style.css'
+import { Github, Linkedin} from 'grommet-icons';
 
 const nolanIcon = 'https://avatars.githubusercontent.com/u/59997535?s=460&u=2afb177181195736519ec2943467b191069bd40d&v=4';
 
 class Profile extends Component {
     render() {
         return (
+            <Box style={{background:"rgba(0,0,0,0.4)"}} round="90px">
             <Grid
                 areas={[
-                    ['main', 'main', 'text', 'text'],
+                    ['main', 'main', 'text', 'blank'],
                     ['about', 'about', 'about', 'about'],
                     ['footer', 'footer', 'footer', 'footer']
                 ]}
@@ -30,15 +32,20 @@ class Profile extends Component {
                     pad="medium"
                     animation={{ type: "fadeIn", duration: "1000" }}
                 >
-                    <Text>My name is Nolan Stucky</Text>
-                    <br />
-                    <Text>I am a:</Text>
+                <div>
+                    <Heading size="22px">My name is <Text color="white" size="25px">Nolan Stucky</Text></Heading>
+                    <br/>
+                    <Heading size="18px" > I am a :</Heading>
+                  
+                    <Heading size="26px" margin={{left:"100px"}} color="white"> Web Developer</Heading>
+                </div>
                 </Box>
 
                 <Box gridArea="about"
                     direction="row"
                     pad="medium"
                     animation={{ type: "fadeIn", duration: "1000" }}
+
                 >
 
                     <Grid
@@ -56,22 +63,25 @@ class Profile extends Component {
                         rows={['25px']}
                         gap="medium"
                         responsive="true">
-
+                    
                         <Text gridArea="text" size="30px" className="text">About Me</Text>
                         <Box width="495px">
-                        <Text gridArea="about" size="20px" className="about" wordBreak="break-word">I am a full stack web developer that absolutly loves the world of technology. I have a passion for learning  </Text>
+                        <Text gridArea="about" size="20px" className="about" wordBreak="break-word" color="white">I am a full stack web developer that absolutly loves the world of technology. I have a passion for learning especially when it comes to things that interest me. Such as full stack web development and game development. Otherwise my hobbies would include snowboarding and surfing. </Text>
                         </Box>
                         <Box width="495px" gridArea='lang'>
                         <Text gridArea="lang" size="25px" className="text" wordBreak="break-word">Skillset:</Text>
                         </Box>
                         <Box width="495px" gridArea='skill'>
-                        <Text gridArea="skill" size="20px" className="about" wordBreak="break-word">Html Css Javascript Jquery Sql Mongo Express React Node Handlebars</Text>
+                        <Text gridArea="skill" size="20px" className="about" wordBreak="break-word" color="white">Html Css Javascript Jquery Sql Mongo Express React Node Handlebars</Text>
                         </Box>
                         <Box width="495px" gridArea='contact'>
                         <Text gridArea="contact" size="25px" className="text" wordBreak="break-word">Contact Me:</Text>
                         </Box>
                         <Box width="495px" gridArea='contactText'>
-                        <Text gridArea="contactText" size="20px" className="about" wordBreak="break-word">Test</Text>
+                        <Text gridArea="contactText" size="20px" className="about" wordBreak="break-word" color="white">Email: Nolan@Stucky.Net</Text>
+                        <Text gridArea="contactText" size="20px" className="about" wordBreak="break-word" color="white">Number: 425-802-7715</Text>
+                        <Text gridArea="contactText" size="20px" className="about" wordBreak="break-word" color="white"><Anchor href="https://github.com/nolanstucky?tab=repositories"  color="white">Github: nolanstucky <Github/></Anchor></Text>
+                        <Text gridArea="contactText" size="20px" className="about" wordBreak="break-word" color="white"> <Anchor href="https://www.linkedin.com/in/nolan-stucky/" color="white">Linkedin: nolanstucky<Linkedin/></Anchor></Text>
                         </Box>
                     </Grid>
                 </Box>
@@ -79,6 +89,7 @@ class Profile extends Component {
 
 
             </Grid>
+            </Box>
         )
     }
 }
